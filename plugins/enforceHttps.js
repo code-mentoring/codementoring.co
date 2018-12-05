87cb2d18b9f7023a55e6c3dbd5dddb8b06cf08da
Tristan Matthias <hello@tristanmatthias.com>
2018-12-05 23:57:26 +1100

    feat: enforcing https on prod

---
+++ plugins/enforceHttps.js
@@ -0,0 +1,16 @@
+const {
+  Route
+} = require('@origami/core');
+
+module.exports = (app, options) => {
+  const r = new Route('*');
+  r.use((req, res, next) => {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  });
+
+  app.useRouter(r);
+}

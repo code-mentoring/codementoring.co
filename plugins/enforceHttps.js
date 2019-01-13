986fb8d15faf3fd0ad271c5c65740d9451e336d2
Tristan Matthias <hello@tristanmatthias.com>
2019-01-13 18:58:32 +1100

    feat: removed enforce of https on local dev

--- plugins/enforceHttps.js
+++ plugins/enforceHttps.js
@@ -3,6 +3,9 @@ const {
 } = require('@origami/core');
 
 module.exports = (app, options) => {
+  // Don't enforce locally
+  if (process.env.NODE_ENV === 'DEVELOPMENT') return;
+
   const r = new Route('*');
   r.use((req, res, next) => {
     // The 'x-forwarded-proto' check is for Heroku
